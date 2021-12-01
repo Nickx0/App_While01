4b5ed9c289820e580d794e266c3d447cd4a4afa1
NickX zXn <gamerplayernick714@hotmail.com>
2021-12-01 15:42:37 -0500

    Agregar archivos de proyecto.

---
+++ App_While01/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace App_While01
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.listNumeros = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(51, 24);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(265, 78);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "Listar los primeros 20 \r\nNumeros naturales";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(51, 126);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(265, 78);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "Listar Numeros pares\r\ndel 100 al 2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(51, 229);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(265, 78);
+            this.btn3.TabIndex = 2;
+            this.btn3.Text = "Listar los Numeros impares \r\ndel 1 al 99\r\n";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // listNumeros
+            // 
+            this.listNumeros.FormattingEnabled = true;
+            this.listNumeros.ItemHeight = 15;
+            this.listNumeros.Location = new System.Drawing.Point(402, 24);
+            this.listNumeros.Name = "listNumeros";
+            this.listNumeros.Size = new System.Drawing.Size(315, 274);
+            this.listNumeros.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(402, 372);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "CANTIDAD :";
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Enabled = false;
+            this.txtCantidad.Location = new System.Drawing.Point(490, 369);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(159, 23);
+            this.txtCantidad.TabIndex = 5;
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Location = new System.Drawing.Point(103, 361);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(163, 36);
+            this.btnCerrar.TabIndex = 6;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listNumeros);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
+            this.Text = "Estructura repetitiva - While";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.ListBox listNumeros;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Button btnCerrar;
+    }
+}
+
---
+++ App_While01/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App_While01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            int num = 1;
+            listNumeros.Items.Clear();
+            while (num <= 20)
+            {
+                listNumeros.Items.Add(num);
+                num++;
+            }
+            txtCantidad.Text = (num - 1).ToString("n0");
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            int num = 100;
+            int cont = 0;
+            listNumeros.Items.Clear();
+            while (num >= 2)
+            {
+                cont++;
+                listNumeros.Items.Add(num);
+                num-=2;
+            }
+            txtCantidad.Text = (cont).ToString("n0");
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            int num = 1,cont = 0;
+            listNumeros.Items.Clear();
+            while (num <= 99)
+            {
+                cont++;
+                listNumeros.Items.Add(num);
+                num++;
+                num++;
+            }
+            txtCantidad.Text = (cont).ToString("n0");
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Desea Salir?", "Alerta", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+    }
+}
